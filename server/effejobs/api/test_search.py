1af921daba2162cfe0c1fa055209b3f2cf7bd677
anshum4n <anshum4n@github>
2018-02-26 11:52:00 +0530

    Added Test Cases

---
+++ server/effejobs/api/test_search.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+from django.test import Client
+
+class SearchTestCase(TestCase):
+
+	def test_search_results(self):
+		c = Client()
+		result = c.get('/api/v1/search/')
+		self.assertEquals(200, result.status_code)
+		result_json = result.json()
+		self.assertEquals(0, result_json['count'])
+
